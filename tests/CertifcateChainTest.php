daded2ac3cf4528cdeac41b64c9a7b5cd3784258
freek <freek@spatie.be>
2016-12-16 11:36:03 +0100

    wip

--- tests/CertifcateChainTest.php
+++ tests/CertifcateChainTest.php
@@ -10,14 +10,14 @@ class CertificateChainTest extends TestCase
     /** @test */
     public function it_can_resolve_a_certificate_chain()
     {
-        $inputFile = __DIR__ . '/fixtures/google/certificate.crt';
+        $inputFile = __DIR__ . '/fixtures/dv-google/certificate.crt';
 
         $certificate = Certificate::loadFromFile($inputFile);
 
         $chainContents = CertificateChain::fetchForCertificate($certificate);
 
         $this->assertEquals(
-            $this->sanitize(file_get_contents(__DIR__ . '/fixtures/google/certificateChain.crt')),
+            $this->sanitize(file_get_contents(__DIR__ . '/fixtures/dv-google/certificateChain.crt')),
             $this->sanitize($chainContents)
         );
     }
--- tests/ResolveCommandTest.php
+++ tests/ResolveCommandTest.php
@@ -7,7 +7,7 @@ class ResolveCommandTest extends TestCase
     /** @test */
     public function it_can_resolve_a_certificate_chain()
     {
-        $inputFile = __DIR__ . '/fixtures/google/certificate.crt';
+        $inputFile = __DIR__ . '/fixtures/dv-google/certificate.crt';
 
         $outputFile = __DIR__ . '/temp/certificateChain.crt';
 
@@ -15,7 +15,7 @@ class ResolveCommandTest extends TestCase
 
         exec("php ./ssl-certificate-chain-resolver resolve {$inputFile} {$outputFile}");
 
-        $certificateChain = __DIR__ . '/fixtures/google/certificateChain.crt';
+        $certificateChain = __DIR__ . '/fixtures/dv-google/certificateChain.crt';
 
         $this->assertFileContentsEqual($certificateChain, $outputFile);
     }
