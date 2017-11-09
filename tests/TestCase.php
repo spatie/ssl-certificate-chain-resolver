0e656e9acfb14572cfc9067c9c2cbde46de84f1c
Gabriel Caruso <carusogabriel34@gmail.com>
2017-11-09 10:31:46 -0200

    Update to PHPUnit 6 (#24)

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,9 @@
 
 namespace Spatie\CertificateChain\Test;
 
-use PHPUnit_Framework_TestCase;
+use PHPUnit\Framework\TestCase as BaseTestCase;
 
-abstract class TestCase extends PHPUnit_Framework_TestCase
+abstract class TestCase extends BaseTestCase
 {
     protected function assertFileContentsEqual($fileA, $fileB)
     {
