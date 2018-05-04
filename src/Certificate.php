0d135919d854508565d6d92949d8043ff6f84002
Freek Van der Herten <freek@spatie.be>
2018-05-04 17:49:59 +0000

    Apply fixes from StyleCI

--- src/Certificate.php
+++ src/Certificate.php
@@ -61,7 +61,7 @@ class Certificate
         return '';
     }
 
-    public function fetchParentCertificate(): Certificate
+    public function fetchParentCertificate(): self
     {
         return static::loadFromUrl($this->getParentCertificateUrl());
     }
